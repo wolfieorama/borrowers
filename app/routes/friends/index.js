d07a045f7acc673d4edd86dfbaf241a830eed181
wolfieorama <wachirawamunyi@gmail.com>
2016-07-06 15:05:48 +0300

    added new route fixed index

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // Here we are using ES6 syntax for functions!
+  // We can use this out of the box with ember-cli
+  // thanks to the addon ember-cli-babel
   model(){
+    // return this.store.findAll('friend');
+    //we can also do store.findRecord or queryRecord to use an ID etc
     return this.store.findAll('friend');
   }
 });
